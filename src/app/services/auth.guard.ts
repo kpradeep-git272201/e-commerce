608b2ccb61e4b7a09a6b1a86441952cf9c8a402d
kpradeep-git272201 <kpradeep.git@gmail.com>
2025-04-22 17:32:45 +0530

    16

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CommonDataService } from './common-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private service: CommonDataService, private router: Router) {}
 
   canActivate(): boolean {
